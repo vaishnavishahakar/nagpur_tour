49010a9e341d739f2a0d9bacf7e7206ee12618de
vaishnavishahakar <vaishnavishahakar1@gmail.com>
2024-08-18 23:14:49 +0530

    created app.js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 function App() {
   return (
     <div>
-      
+      <h1>Nagpur</h1>
+
+      <img src='https://nagpurtourism.co.in/images/places-to-visit/header/deeksha-bhoomi-nagpur-tourism-entry-fee-timings-holidays-reviews-header.jpg' 
+      alt='diksha-bhumi-img' height='200px' />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <h1>hello nagpur</h1>
+    <App />
 );
 

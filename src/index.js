1bf8a5fb9c10d256e8f00b761bb002dd6502054f
vaishnavishahakar <vaishnavishahakar1@gmail.com>
2024-08-18 23:38:43 +0530

    render files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import About from './About';
 import Contact from './Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Home/>
-);
 
+const currentPath = window.location.pathname;
+
+if (currentPath=="/"){
+  root.render(<Home/>)
+}
+else if(currentPath=="/about")
+{
+  root.render(<About/>)
+}
+else if(currentPath=="/contact")
+{
+  root.render(<Contact/>)
+}
+else{
+  root.render(<h1>404 Not Found</h1>)
+}
